348375fa67fa4cb0a5b750564238b5c47f911b3e
Andrew Loucky <andrew.loucky@gmail.com>
2014-01-19 13:38:29 -0500

    pset1/mario.c, complete

---
+++ pset1/mario.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int height, num_blanks, num_xs, base, i;
+
+    while ( height <= 0 || height > 23 ){
+        printf("Height: ");
+        height = GetInt();
+        if ( height == 0 ) {
+            return 0;
+        }
+    }
+
+    num_blanks = height - 1;
+    num_xs = 2;
+    base = height + 1;
+
+    // print pyramid down to (and excluding) the base
+    while(num_blanks != 0) {
+        for( i = num_blanks; i > 0; i-- ) {
+            printf(" ");
+        }
+        for ( i = num_xs; i > 0; i-- ) {
+            printf("#");
+        }
+        printf("\n");
+
+        num_blanks--;
+        num_xs++;
+    }
+
+    // print base
+    for ( i = base; i > 0; i-- ){
+        printf("#");
+    }
+
+    printf("\n");
+}
+
